1d8acf3b2f7c061fea38b622a838556d858c8e3f
HoangLong <pdhlong933@gmail.com>
2023-11-01 17:07:33 +0700

    vercel

--- app.js
+++ app.js
@@ -43,12 +43,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use((req, res, next) => {
-//     const error = new Error('Not found');
-//     error.status = 404;
-//     next(error);
-// });
-
 app.use((error, req, res, next) => {
   res.status(error.status || 500).json({
     message: error,
